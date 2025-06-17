7cf2908699c54c7766a10e65500bed1a87443822
gibran <zaga1709@hotmail.com>
2025-06-16 21:00:02 -0600

    Subimos el proyecto

---
+++ manager/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path 
+from . import views 
+
+urlpatterns = [
+    path('', views.bienvenida, name='bienvenida'),
+    path('iniciar_sesion/', views.iniciar_sesion, name='iniciar_sesion'),
+    path('registrar/', views.registrar, name='registrar'),
+    path('menu/', views.menu, name='menu'),
+    path('cerrar_sesion/', views.cerrar_sesion, name='cerrar_sesion'),
+]
---
+++ manager/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import RegistroForm
+
+# Create your views here.
+def bienvenida(request):
+    return render(request, 'manager/bienvenida.html')
+
+def registrar(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('iniciar_sesion')
+    else:
+        form = RegistroForm()
+    return render(request, 'manager/registrar.html', {'form':form})
+
+
+def iniciar_sesion(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('menu')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'manager/iniciar_sesion.html', {'form':form})
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('bienvenida')
+
+def menu(request):
+    return render(request, 'manager/menu.html')

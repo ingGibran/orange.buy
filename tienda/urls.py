9d9679eef2e87a3d35ef0c44064eba96adb81c2a
gibran <zaga1709@hotmail.com>
2025-08-21 21:34:03 -0600

    mis ventas y menu

--- tienda/urls.py
+++ tienda/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('menu/', views.menu_tienda, name='menu_tienda'),
     path('vender/', views.vender, name='vender'),
-    path('mis_ventas/', views.mis_ventas, name='mis_ventas')
+    path('mis_ventas/', views.mis_ventas, name='mis_ventas'),
+    
+    path('pausar/<int:venta_id>/', views.pausar_venta, name='pausar_venta'),
+    path('reanudar/<int:venta_id>/', views.reanudar_venta, name='reanudar_venta'),
+    path('eliminar/<int:venta_id>/', views.eliminar_venta, name='eliminar_venta'),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import AgregarProductoForm
-from .models import Producto
+from .models import Producto, Producto
 
 # Create your views here.
 
 @login_required
 def menu_tienda(request):
-    return render(request, 'tienda/menu.html')
+    valencia = Producto.objects.filter(activo=True, tipo='valencia')
+    navel = Producto.objects.filter(activo=True, tipo='navel')
+    sanguinea = Producto.objects.filter(activo=True, tipo='sanguinea')
+    otra = Producto.objects.filter(activo=True, tipo='otra')
+    return render(request, 'tienda/menu.html', {'valencia':valencia, 'navel':navel, 'sanguinea': sanguinea, 'otra':otra})
 
 @login_required
 def vender(request):
@@ -27,3 +31,24 @@ def vender(request):
 def mis_ventas(request):
     ventas = Producto.objects.filter(vendedor=request.user)
     return render(request, 'tienda/mis_ventas.html', {'ventas':ventas})
+
+@login_required
+def pausar_venta(request, venta_id):
+    producto = get_object_or_404(Producto, id=venta_id, vendedor=request.user)
+    producto.activo = False
+    producto.save()
+    return redirect('mis_ventas')
+
+@login_required
+def reanudar_venta(request, venta_id):
+    producto = get_object_or_404(Producto, id=venta_id, vendedor=request.user)
+    producto.activo = True
+    producto.save()
+    return redirect('mis_ventas')
+
+@login_required
+def eliminar_venta(request, venta_id):
+    producto = get_object_or_404(Producto, id=venta_id, vendedor=request.user)
+    producto.delete()
+    return redirect('mis_ventas')
+    
